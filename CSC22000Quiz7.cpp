20f8e725c2f17acce23ff0d4b5ea57bf195f211d
Vaneat01 <vanealmonte01@gmail.com>
2025-11-17 18:02:40 -0500

    Update CSC22000Quiz7.cpp

--- CSC22000Quiz7.cpp
+++ CSC22000Quiz7.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 class Solution{
     public:
-        int maxProfit(vector<int>& prices) {    //creates a vector that stores the stocks
+        int maxProfit(vector<int>& prices) {    //create a vector that stores the stocks
             int max=0;  //initialize max profit at 0
             int start = prices[0];  //start at index 0
             int len = prices.size();    //set length to be the number of elements in the vector
             for(int i=1;i<len;i++){ //traverse through the vector
-                if (start < prices[i]){ //if the current stock we own is < the stock at current i.
+                if (start < prices[i]){ //if the current stock we own is < the stock at current i
                     max += prices[i] - start;   //make profit->store in max
                 }
                 start = prices[i];  //start new, and next element becomes current stock
@@ -17,5 +17,6 @@ class Solution{
             return max;
         }
 };
+
 
 //[7, 1, 5, 3, 6, 4]
